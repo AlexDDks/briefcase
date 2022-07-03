f4df7bfba53d73e3acf1f79845cf150f1b7a8475
AlexDDks <magarciaa92@gmail.com>
2022-07-02 21:51:04 -0500

    añadiendo mejoras visuales, páginas de fotos y music

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,12 @@
 const controller = {
     index: (req, res) => {
         res.render('index')
+    },
+    photos: (req, res) => {
+        res.render('photos')
+    },
+    music: (req, res) => {
+        res.render('music')
     }
 }
 
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index)
+router.get('/photos', mainController.photos)
+router.get('/music', mainController.music)
 
 //Aun no entiendo porque se exporta la variable router
 module.exports = router;
