1473b67433942ba720c1b011f7e16aed30354d09
AlexDDks <magarciaa92@gmail.com>
2022-07-06 23:16:09 -0500

    subiendo slider photos

--- slider.js
+++
@@ -1,9 +0,0 @@
-const slider = document.querySelector("#slider")
-let sliderSection = document.querySelectorAll(".sliderSection")
-let sliderSectionLast = sliderSection[sliderSection.length - 1]
-
-const sliderButtonRight = document.querySelector("#sliderButtonRight")
-const sliderButtonLeft = document.querySelector("#sliderButtonLeft")
-
-slider.insertAdjacentElement("afterbegin", sliderSectionLast)
-
